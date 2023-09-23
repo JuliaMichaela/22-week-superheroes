6346d87e187cd917647cf95c4cb861b8958fa8c1
JuliaMichaela <juliaperez@yandex.ru>
2023-09-23 22:52:48 +0300

    created component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import './style/App.css';
+import heroes from './heroes.json'
+import Hero from './components/Hero';
+
+
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className='heroes-container'>
+      {heroes.map((hero, index) => (
+        <Hero name = {hero.name}
+        universe = {hero.universe}
+        alterego = {hero.alterego}
+        occupation = {hero.occupation}
+        friends = {hero.friends}
+        superpowers = {hero.superpowers}
+        url = {hero.url}
+        info = {hero.info}
+        key ={index} />
+      ))}
     </div>
   );
 }
